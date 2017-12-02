1b4e01bd817d97e489daf331963a9b38e442fe2e
Oleksii Bespalko <olbesp365@gmail.com>
2017-12-02 21:56:19 +0200

    Fix the query url

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $('#btn-next-quote').on('click', function(e) {
     e.preventDefault();
     $.ajax( {
-      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=',
+      url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=',
       success: function(data) {
         var post = data.shift(); // The data is an array of posts. Grab the first one.
         $('#quote-title').text(post.title);
