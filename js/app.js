92b7f734f2c7dde3814054fc96b485d4edd51502
Oleksii Bespalko <olbesp365@gmail.com>
2017-12-03 13:03:09 +0200

    Show quote when the web page loads

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
 
-  $('#btn-next-quote').on('click', function(e) {
-    e.preventDefault();
+  var getNextQuote = function() {
     $.ajax( {
       url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=',
       success: function(data) {
         var post = data.shift(); // The data is an array of posts. Grab the first one.
         $('#quote-title').html(post.title);
         $('#quote-content').html(post.content);
+
+
 
         // If the Source is available, use it. Otherwise hide it.
         if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
@@ -18,6 +19,18 @@ $(document).ready(function() {
       },
       cache: false
     });
+  };
+
+  getNextQuote();
+
+  $('#btn-next-quote').on('click', function(e) {
+
+    e.preventDefault();
+    getNextQuote();
+
   });
 
+
+
 });
+  // $('.quote-box').addClass('quote-fadeIn');
